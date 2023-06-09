6110d126cf5ff1f0e7f1412f9787aa6e32a28ad8
Naveen <abdenavin1724@gmail.com>
2023-06-09 15:02:54 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    eid=models.IntegerField()
+    ename=models.CharField(max_length=100)
+    eage=models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.ename
---
+++ app/serializers.py
@@ -0,0 +1,6 @@
+from app.models import *
+from rest_framework import serializers
+class  EmployeeMS(serializers.ModelSerializer):
+    class Meta:
+        model=Employee
+        fields='__all__'
